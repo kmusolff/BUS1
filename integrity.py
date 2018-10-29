27eb81bca9a43c2f39d2df8db722c2fb4831b9f3
pgaska <pawegska@ymail.com>
2018-10-29 18:47:17 +0100

    zmiany do integrity

--- integrity.py
+++ integrity.py
@@ -1,30 +1,41 @@
 import hashlib
 
 class Integrity:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def _hash(self, bytes):
-		hash_obj = hashlib.sha3_256()
-		hash_obj.update(bytes)
-		return hash_obj.digest()
+    def _hash(self, bytes):
+        hash_obj = hashlib.sha3_256()
+        hash_obj.update(bytes)
+        return hash_obj.digest()
 
-	def hash_file(self, file_from):
-		with open(file_from, "rb") as file:
-			data = file.read()
+    def hash_file(self, file_from):
+        with open(file_from, "rb") as file:
+            data = file.read()
 
-		return self._hash(data)
+        return self._hash(data)
 
-	def hash_to_file(self, file_from, file_to):
+    def hash_to_file(self, file_from, file_to):
 
-		hashed_file = self.hash_file(file_from)
+        hashed_file = self.hash_file(file_from)
 
-		with open(file_to, "wb") as file:
-			file.write(hashed_file)
+        with open(file_to, "wb") as file:
+            file.write(hashed_file)
 
+    def integrity_check(self, file1, file2):
+        with open(file1, "rb") as file:
+            data1 = file.read()
 
-	def integrity_check(file1, file2):
-		return self.hash_file(file1) == self.hash_file(file2)
+        hash1 = self._hash(data1)
+        print(hash1)
 
+        with open(file2, "rb") as file:
+            hash2 = file.read()
+        print(hash2)
+
+        if hash1 == hash2:
+            print("Funkcja skrótu odpowiada podanej wartości")
+        else:
+            print("Funkcja skrótu nie odpowiada podanej wartości")
 
 
